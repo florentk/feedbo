93ad061015291b8bf7820495033b8a99a515c9e5
Florent Kaisser <florent@serveur.kaisser.name>
2016-08-08 10:10:04 +0200

    do not show source for hightlight feed exepted for google news

--- src/feed.php
+++ src/feed.php
@@ -1,14 +1,16 @@
 <?php
 	$user = $_POST["user"];
 	$password = $_POST["password"];
+	$t = time();
 ?>
 
 
 <html>
 <head>
-	<script src="https://code.jquery.com/jquery-3.1.0.js"></script>
-	<script src="ressources/js/tt-rss.js"></script>
-	<script src="ressources/js/feed.js"></script>
+	<meta http-equiv="Cache-control" content="no-cache">
+	<script src="ressources/js/jquery.js"></script>
+	<script src="ressources/js/tt-rss.js?t=<?=$t?>"></script>
+	<script src="ressources/js/feed.js?t=<?=$t?>"></script>
 </head>
 <body onload='login("<?=$user?>","<?=$password?>");'>
 
--- src/ressources/js/feed.js
+++ src/ressources/js/feed.js
@@ -11,36 +11,56 @@ function get_feeds_from_cat_id (sid, cb) {
 
 }
 
-function filter_feed_entry (e) {
-	if (e.link.startsWith("http://news.google.com")){
-		//console.log("feed title : " + e.feed_title);
-		const begin = e.title.lastIndexOf(" - ");
-		const end = e.title.length - 1;
-		return {"title" : e.title.substr(0, begin), 
-				"source" : e.title.substr(begin + 3, end), 
-				"link" : e.link
-		};
-	}
+function is_source_from_title(link) {
+	return link.startsWith("http://news.google.com");
+}
+
+function entry_from_title(e) {
+	const begin = e.title.lastIndexOf(" - ");
+	const end = e.title.length - 1;
+	return {"title" : e.title.substr(0, begin), 
+			"source" : e.title.substr(begin + 3, end), 
+			"link" : e.link
+	};
+}
+
+function entry_default(e) {
 	return {"title" : e.title, 
 			"source" : e.feed_title, 
 			"link" : e.link
 	};
 }
 
-function entry_to_html () {
+function filter_feed_entry (e, from_title) {
+	if (from_title)
+		return entry_from_title(e);
+	else
+		return entry_default(e);
+}
+
+function source_to_html (e){
+		return '<br>\n\r\r<small>' + e.source + '</small>\n';
+}
+
+function title_to_html (e){
+	return '\r\r<a target="_blank" href="' + e.link +'">' + e.title + '</a>';
+}
+
+function entry_to_html (show_source) {
 	return function(entry){
-		const e = filter_feed_entry(entry);
-		return	'\r<li><a target="_blank" href="' + e.link +'">' + e.title + '</a>'
-				+'<br>\n'
-				+'\r<small>' + e.source + '</small>\n' 
+		const from_title = is_source_from_title(entry.link);
+		const e = filter_feed_entry(entry,from_title);
+		return	'\r<li>\n'
+				+ title_to_html(e)
+				+ ((show_source || from_title) ? source_to_html(e) : "")
 				+'\r</li>\n'; 
 	} 
 }
 
-function write_entries (id) {
+function write_entries (id, show_source) {
 	return function ( data ) {
 		const entries = data.content;
-		const html_entries = entries.map(entry_to_html());
+		const html_entries = entries.map(entry_to_html(show_source));
 		$("p#" + id).html("<ul>\n" + html_entries.join("\n") + "</ul>");
 	}
 }
@@ -57,13 +77,13 @@ function write_highlight_entries (sid) {
 
 		feeds.forEach(function (f, i) { 
 			const id = f.id;
-			tt_rss_get_headline (sid, id, 10, write_entries("hl"+i));
+			tt_rss_get_headline (sid, id, 10, write_entries("hl"+i, false));
 		})	;
 	}
 }
 
 function load (sid) {
-	tt_rss_get_headline (sid, -3, 30, write_entries("fresh"));
+	tt_rss_get_headline (sid, -3, 30, write_entries("fresh", true));
 	get_feeds_from_cat_id(sid, write_highlight_entries(sid));
 }
 
